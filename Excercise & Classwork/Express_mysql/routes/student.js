1ce094467784a07168b3c259875930b1353adab6
Sakshi Kamble <sakshikamble300@gmail.com>
2025-12-18 09:33:13 +0530

    git ignore file

--- Excercise & Classwork/Express_mysql/routes/student.js
+++ Excercise & Classwork/Express_mysql/routes/student.js
@@ -9,19 +9,35 @@ router.get('/',(request,response)=>{
     pool.query(sql,(error,data)=>{
         response.send(result.createResult(error,data))
     })
-})
+});
 
-router.post('/',(request,response)=>{
+router.post("/", (request, response) => {
     // Destructing
-    const {name,email,course}=request.body
-    const sql=`Insert into students(name,email,course) Values(?,?,?)`
-    pool.query(sql,[name,email,course],(error,data)=>{
+    const { name, email, course } = request.body
+    const sql = `INSERT INTO student(name,email,course) VALUES(?,?,?)`
+    pool.query(sql, [name, email, course], (error, data) => {
+        response.send(result.createResult(error, data))
+    })
+
+});
+
+// Todo
+router.put('/',(request,response)=>{
+    // Destructing
+    const{course,id}=request.body
+    const sql=`Update student set course=? where id=?`
+    pool.query(sql,[course,id],(error,data)=>{
+        response.send(result.createResult(error,data))
+    })
+});
+
+router.delete('/',(request,response)=>{
+    // Destructing
+    const {id} =request.body
+    const sql=`Delete from student where id=?`
+    pool.query(sql,[id],(error,data)=>{
         response.send(result.createResult(error,data))
     })
 })
-
-// Todo
-router.put()
-router.delete()
 
 module.exports=router
--- Excercise & Classwork/Express_mysql/server.js
+++ Excercise & Classwork/Express_mysql/server.js
@@ -5,8 +5,11 @@ const PORT=5000
 const studentRouter=require('./routes/student')
 
 // Middle ware 
+app.use(express.json())
+
 app.use('/student',studentRouter)
 
-app.listen(PORT,'localhost',()=>{
+// http://localhost:5000/student
+app.listen(PORT,()=>{
     console.log("Server started at port",PORT);
 });
