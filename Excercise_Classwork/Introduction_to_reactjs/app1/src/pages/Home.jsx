2ac3b1255d3033bf1d35667198e3205aa10bcadd
Sakshi Kamble <sakshikamble300@gmail.com>
2025-12-23 12:18:24 +0530

    Some file added and changes done

---
+++ Excercise_Classwork/Introduction_to_reactjs/app1/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+
+// functionl component
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <Navbar/>
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home
+
---
+++ Excercise_Classwork/Introduction_to_reactjs/app1/src/pages/Orders.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+
+// functional components
+import React from 'react'
+
+function Orders() {
+  return (
+    <div>
+      <Navbar/>
+      <h1>Orders</h1>
+      
+    </div>
+  )
+}
+
+export default Orders
